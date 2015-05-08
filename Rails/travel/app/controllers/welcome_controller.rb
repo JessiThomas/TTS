fb482b062239bbb0bee4199b35f57493e0da5e9f
Jessica Thomas <jessicathomas@Jessicas-MacBook-Pro.local>
2015-05-08 11:14:13 -0400

    travel changes

--- Rails/travel/app/controllers/welcome_controller.rb
+++ Rails/travel/app/controllers/welcome_controller.rb
@@ -2,11 +2,15 @@ class WelcomeController < ApplicationController
   def index
   	@homeland = 'Italy'
   	@countries = ['Czech Republic', 'Spain', 'Turkey', 'Russia']
-  	@travel_pics = ['CzechRepublic.jpg', 'Italy.jpg', 'Russia.jpg', 'Spain.jpg', 'Turkey.jpg']
+  	@travel_pics = ['CzechRepublic.jpg', 'Italy2.jpg', 'Russia.jpg', 'Spain.jpg', 'Turkey.jpg']
   end
 
   def about
 	 @color = params[:color]  
 	 @size = params[:size].to_i
   end
+
+  def contact
+  end
+
 end
